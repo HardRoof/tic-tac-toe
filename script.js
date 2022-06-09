dd35ac90e8079b027b9bb72d447294016574d295
HardRoof <mateus_grandez@hotmail.com>
2022-06-09 00:11:27 -0600

    Created game's logic

---
+++ script.js
@@ -0,0 +1,124 @@
+const gameBoardContainer = document.querySelector("#gameBoardContainer")
+const body = document.body
+const container = document.querySelector("#container")
+let gameBoard = ["", "", "","", "", "","", "", ""]
+
+const GameBoard = (() => {
+  const createGameBoard = () => {
+    const NumberOfCells = 9
+    for (let i = 0; i < NumberOfCells; i++) {
+      cells = document.createElement('div');
+      gameBoardContainer.appendChild(cells);
+      cells.setAttribute("name", "nameCells");
+      cells.dataset.position = i
+      cells.classList.add('classCells');
+      cells.setAttribute("onclick", "attack(this)")
+    }
+    classCells = document.getElementsByName('nameCells'); //PS: NodeList and live
+  };
+
+  const player = (name, weapon) => {
+    let attacked = false
+    return {name, weapon, attacked};
+  };
+  const user = player('user', "X");
+  const computer = player('computer', "O");
+
+  const playRound = (e) => {
+    userAttack(e)
+    computerAttack(e)
+  }
+
+  const userAttack = (e) => {
+    if ((e.innerText == "") && (user.attacked == false)) {
+      user.attacked = true
+      gameBoard.splice(arrayPosition, 1, user.weapon);
+      e.innerText = user.weapon
+      checkWinner()
+    }
+  }
+
+  const computerAttack = (e) => {
+    if ((e.innerText == "") && (user.attacked == true)) {
+      user.attacked = false
+      gameBoard.splice(arrayPosition, 1, computer.weapon);
+      e.innerText = computer.weapon
+      checkWinner()
+    }
+  }
+
+  const checkWinner = () => {
+    for (let i = 0; i < 7; i+=3) {
+      if ((gameBoard[i] == gameBoard[i+1]) && (gameBoard[i] == gameBoard[i+2]) && (gameBoard[i] != "")) {
+        console.log("Won")
+        gameOver()
+      }
+    }
+    for (let i = 0; i < 3; i++) {
+      if ((gameBoard[i] == gameBoard[i+3]) && (gameBoard[i] == gameBoard[i+6]) && (gameBoard[i] != "")) {
+        console.log("Won")
+        gameOver()
+      }
+    }
+    if (gameBoard[0] == gameBoard[4] && gameBoard[0] == gameBoard[8] && gameBoard[0] != "") {
+      console.log("Won")
+      gameOver()
+    }
+    if (gameBoard[2] == gameBoard[4] && gameBoard[2] == gameBoard[6] && gameBoard[2] != "") {
+      console.log("Won")
+      gameOver()
+    }
+  }
+
+  const gameOver = () => {
+    classCells.forEach(div => {
+      div.removeAttribute("onclick")
+    });
+    scoreBoard = document.createElement('div');
+    scoreBoard.id = "scoreBoardID"
+    container.appendChild(scoreBoard)
+    scoreBoard.appendChild(document.createTextNode("The winner is")) 
+
+    newSpan = document.createElement('SPAN')
+    scoreBoard.appendChild(newSpan)
+    if (user.attacked == true) {
+      newSpan.innerText = `${user.weapon}`
+    }
+    else {
+      newSpan.innerText = `${computer.weapon}`
+    }
+    container.classList.add('blur');
+
+    restartBtn = document.createElement('button');
+    scoreBoard.appendChild(restartBtn)
+    restartBtn.type = "button"
+    restartBtn.textContent = "Play again"
+    restartBtn.addEventListener("click", () => {
+      container.classList.remove("blur");
+      scoreBoard.remove()
+      gameBoard = ["", "", "","", "", "","", "", ""]
+      classCells.forEach(div => {
+        div.innerText = ""
+      });
+      classCells.forEach(div => {
+        div.setAttribute("onclick", "attack(this)")
+      });
+    user.attacked = false
+    })
+    
+  }
+
+  return {
+    createGameBoard,
+    playRound,
+    player,
+    user,
+    computer,
+  }
+})();
+
+GameBoard.createGameBoard()
+function attack(e) {
+  arrayPosition = e.getAttribute("data-position")
+  GameBoard.playRound(e)
+}
